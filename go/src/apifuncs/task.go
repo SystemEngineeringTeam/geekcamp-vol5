dc89d89e095d3b0fb9184a1c97c6bfece2486d42
SuperTikuwa <tikuwamk2@gmail.com>
2021-07-03 14:26:48 +0900

    add: /taskのgetを追加

--- go/src/apifuncs/task.go
+++ go/src/apifuncs/task.go
@@ -3,11 +3,14 @@ package apifuncs
 import "github.com/gin-gonic/gin"
 
 type JsonTaskGet struct {
-	TaskID      int
-	Detail      string
-	IsAvailable bool
+	TaskID      int    `json:"task_id,omitempty"`
+	Detail      string `json:"detail,omitempty"`
+	IsAvailable bool   `json:"is_available,omitempty"`
 }
 
 func TaskGetHandler(c *gin.Context) {
+	var json JsonTaskGet
+	if err:=c. {
 
+	}
 }

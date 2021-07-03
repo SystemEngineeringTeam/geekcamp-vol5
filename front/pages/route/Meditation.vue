f384616a8c2c585ef22e1f3526e26e0d3d00a9b3
hihumikan <musecharo@gmail.com>
2021-07-03 22:30:15 +0900

    1枚目　皮だけ

--- front/pages/route/Meditation.vue
+++ front/pages/route/Meditation.vue
@@ -1,3 +1,74 @@
 <template>
-  <div class="black--text text-h4">瞑想をする</div>
+  <body>
+    <center class="all">
+      <p color="black" class="texttmp">何分間瞑想しますか？</p>
+      <v-row>
+        <v-col>
+          <v-btn
+            large
+            elevation="4"
+            height="5.5rem"
+            width="12rem"
+            style="text-transform: none"
+          >
+            <div class="font">2min</div>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+            large
+            elevation="4"
+            height="5.5rem"
+            width="12rem"
+            style="text-transform: none"
+          >
+            <div class="font">4min</div>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+            large
+            elevation="4"
+            height="5.5rem"
+            width="12rem"
+            style="text-transform: none"
+          >
+            <div class="font">6min</div>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+            large
+            elevation="4"
+            height="5.5rem"
+            width="12rem"
+            style="text-transform: none"
+          >
+            <div class="font">8min</div>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </center>
+  </body>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.texttmp {
+  color: black;
+}
+.all {
+  margin-top: 10px;
+  font-size: 7.7vh;
+}
+.font {
+  font-size: 32px;
+  font-weight: bold;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>

96c1a2a1b53ab21d16046af864f2554b17ddb863
hihumikan <musecharo@gmail.com>
2021-07-03 17:15:22 +0900

    sql insert

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -12,3 +12,23 @@ use backend_db;
     count INT NOT NULL,
     FOREIGN KEY(id) REFERENCES tasks(id) ON DELETE CASCADE
 );
+
+insert into
+   tasks(id,detail,isAvaliable)
+values
+   (0,"運動させる",1);
+
+insert into
+   tasks(id,detail,isAvaliable)
+values
+   (1,"音楽を流す",1);
+
+insert into
+   tasks(id,detail,isAvaliable)
+values
+   (2,"報酬を与える",0);
+
+insert into
+   tasks(id,detail,isAvaliable)
+values
+   (3,"瞑想させる",0);

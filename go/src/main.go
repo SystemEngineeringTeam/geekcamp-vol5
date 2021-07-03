5e7d51456142f4db7278107635a2428ba1628230
hihumikan <musecharo@gmail.com>
2021-07-03 17:08:48 +0900

    main.go

--- go/src/main.go
+++ go/src/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"github.com/SystemEngineeringTeam/geekcamp-vol5/apifuncs"
-	"github.com/SystemEngineeringTeam/geekcamp-vol5/dbctl"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	gin.SetMode(gin.DebugMode)
-	dbctl.Test()
-	dbctl.Gettasks()
+
 	r.GET("/task", apifuncs.TaskGetHandler)
 	r.POST("/task", apifuncs.PostTaskHandler)
 	r.Run(":80")
